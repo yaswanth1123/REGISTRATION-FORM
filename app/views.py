28e1125bcf6d948b073b4e8ea771b88dfda2a0e1
hanumanthk1128 <khanumanth.msbi@gmail.com>
2022-07-22 18:53:29 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
+import profile
 from django.shortcuts import render
 from app.forms import ProfileForm, UserForm
+from django.http import HttpResponse
 
 # Create your views here.
 from app.models import *
@@ -8,5 +10,25 @@ def registration(request):
     UF=UserForm()
     PF=ProfileForm()
     d={'UF':UF,'PF':PF}
-    
+    if request.method=='POST' and request.FILES:
+        UD=UserForm(request.POST)
+        PD=ProfileForm(request.POST,request.FILES)
+        if UD.is_valid() and PD.is_valid():
+            user1=UD.save(commit=False)
+            user1.set_password(UD.cleaned_data['password'])
+            user1.save()
+            profile=PD.save(commit=False)
+            profile.user=user1
+            profile.save()
+
+            send_mail('registration',
+            'Registration is successfull',
+            'yaswanthreddyyash7660@gmail.com',
+            [user1.email],
+            fail_silently=True)
+
+            return HttpResponse('resigration is successful')
+            
+
+        
     return render(request,"registration.html",d)
--- projectyash19/settings.py
+++ projectyash19/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from smtplib import SMTP
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +129,13 @@ MEDIA_URL='media/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_HOST_USER='yaswanthreddyyash7660@gmail.com'
+EMAIL_HOST_PASSWORD='yashharry'
+EMAIL_PORT=586
+EMAIL_USE_SSL=False
+EMAIL_USE_TLS=True
